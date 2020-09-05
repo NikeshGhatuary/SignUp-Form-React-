2e5cb28ca5421013e2b2a2b44b9384fccac5f89f
NikeshGhatuary <51503971+NikeshGhatuary@users.noreply.github.com>
2020-09-05 19:30:54 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import GoogleLogin from "react-google-login";
 import FacebookLogin from "react-facebook-login";
+import { BrowserRouter } from "react-router-dom";
 
 //import logo from './logo.svg';
 import "./App.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-//import "bootstrap-css-only/css/bootstrap.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import {
   MDBContainer,
@@ -18,6 +19,7 @@ import {
   MDBBtn,
   MDBIcon,
   MDBModalFooter,
+  MDBNav,
   MDBNavbar,
   MDBNavbarBrand,
   MDBNavbarNav,
@@ -35,13 +37,29 @@ import {
 const FormPage = () => {
   return (
     <MDBContainer>
+      <BrowserRouter>
+        <MDBNav className="justify-content-center">
+          <MDBNavItem>
+            <MDBNavLink active to="#!">
+              <img
+                style={({ height: 60 + "px" }, { marginBottom: 5 + "px" })}
+                src="./logo_1.png"
+                alt="Online Travel Agency"
+                title="Lowest Hotel Fairs in India"
+                to="http://localhost:3000"
+              />
+            </MDBNavLink>
+          </MDBNavItem>
+        </MDBNav>
+      </BrowserRouter>
+      <MDBModalFooter className="mx-20 pt-0 mb-1"></MDBModalFooter>
       <MDBRow>
         <MDBCol md="10">
           <MDBCard className="cardform">
             <div className="text-center">
               <h5
                 className="dark-grey-text mb-5"
-                style={{ marginTop: 1 + "em" }}
+                style={{ marginTop: 0 + "px" }}
               >
                 <strong>SIGN UP</strong>
               </h5>
